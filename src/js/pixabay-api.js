b9a741f7f78e9233a991183f122af1c07a3b81e9
Serhii Yemets <Serhiiyemets2020@gmail.com>
2025-10-21 08:24:59 +0200

    add paxabay file

---
+++ src/js/pixabay-api.js
@@ -0,0 +1,34 @@
+import axios from 'axios';
+
+const API_KEY = '52724963-1b39cb743cbbccbbd6e65dea0';
+const BASE_URL = 'https://pixabay.com/api/';
+
+export async function getImageByQuery(query, page) {
+    try {
+        const response = await axios.get(BASE_URL, {
+            params: {
+                key: API_KEY,
+                q: query,
+                image_type: 'photo',
+                orientation: 'horizontal',
+                safesearch: true,
+                page,
+                per_page: 15,
+            }
+        });
+
+        if (!response.data.hits) {
+            throw new Error('Неожиданный формат ответа API');
+        }
+
+        return response.data;
+    }
+
+    catch (error) {
+        console.error('Error:', error);
+        throw error;
+    }
+}
+
+
+
